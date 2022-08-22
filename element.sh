9b499e263390849b108392d5526f1162b457cc37
saeedorsaid <sabernia.saeed@gmail.com>
2022-08-22 11:13:58 +0000

    feat: add condition to check if argument is a name

--- element.sh
+++ element.sh
@@ -28,6 +28,18 @@ then
       echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($1). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."	
     done     
   fi
+  # check if argument is an element name
+  if [[ $1 =~ ^[A-Z][a-z][a-z]+ ]]
+  then 
+    ATOMIC_NUMBER=$(echo $($PSQL "SELECT atomic_number FROM elements WHERE name='$1'") | sed 's/ //g')
+    SYMBOL=$(echo $($PSQL "SELECT symbol FROM elements WHERE name='$1'") | sed 's/ //g')
+    INFO=$($PSQL "SELECT atomic_mass, melting_point_celsius, boiling_point_celsius, type_id FROM properties WHERE atomic_number=$ATOMIC_NUMBER")
+    echo "$INFO" | while read ATOMIC_MASS BAR MELTING_POINT BAR BOILING_POINT BAR TYPE_ID
+    do 
+      TYPE=$(echo $($PSQL "SELECT type FROM types WHERE type_id=$TYPE_ID") | sed 's/ //g')
+      echo "The element with atomic number $ATOMIC_NUMBER is $1 ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $1 has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."	
+    done    
+  fi
 
 else
   echo Please provide an element as an argument.
